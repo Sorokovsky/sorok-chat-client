b0d6b89b0fc5ed085ed302216734441f3913d6f1
Sorokovsky <Sorokovskys@ukr.net>
2025-01-01 23:42:49 +0200

    load images from server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import React, {FC, JSX, ReactNode} from "react";
 import "@/styles/global.sass"
 import {Provider} from "@/components/provider";
 import {Header} from "@/components/header/header";
+import {EnvironmentKeys} from "@/enums/environment-keys.enum";
+import {getServerURL} from "@/utils/backand";
 
 export const metadata: Metadata = {
   title: {
@@ -11,13 +13,17 @@ export const metadata: Metadata = {
   },
   description: "Best ukrainian messenger",
 };
-
+const server: string = getServerURL(
+    process.env[EnvironmentKeys.SERVER_PROTOCOL]!,
+    process.env[EnvironmentKeys.SERVER_HOST]!,
+    process.env[EnvironmentKeys.SERVER_PORT]!,
+)
 const RootLayout: FC<Readonly<{children: ReactNode;}>> = ({children}): JSX.Element => {
   return (
       <html lang={"en"} suppressHydrationWarning>
       <body>
           <Provider>
-              <Header />
+              <Header server={server} />
               {children}
           </Provider>
       </body>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,24 @@
-import {type FC, type JSX} from "react";
+"use client"
+import {type FC, type JSX, useEffect, useState} from "react";
 import cn from 'clsx';
 import styles from "@/components/header/header.module.sass"
+import Image from "next/image";
+import {useProfile} from "@/hooks/use-profile.hook";
 
-export const Header: FC = (): JSX.Element => {
+export const Header: FC<{server: string}> = ({server}): JSX.Element => {
+    const { data } = useProfile();
+    const [path, setPath] = useState<string | undefined>(undefined);
+    useEffect(() => {
+        if(data?.avatarPath !== undefined) {
+            setPath(server + "/" + data?.avatarPath);
+            console.log(path);
+        }
+    }, [data]);
     return (
         <header
             className={cn(styles.header)}
-        ></header>
+        >
+            {path && <Image src={path} alt={"avatar"} width={50} height={50} priority />}
+        </header>
     );
 }
