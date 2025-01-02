faa613f7a833fccb7591be8d73a587596ba83817
Sorokovsky <Sorokovskys@ukr.net>
2025-01-02 23:33:23 +0200

    refactor

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,16 +4,14 @@ import cn from 'clsx';
 import styles from "@/components/header/header.module.sass"
 import {useProfile} from "@/hooks/use-profile.hook";
 import {Avatar} from "@/components/avatar/avatar";
-import {useLogout} from "@/hooks/use-logout";
 
 export const Header: FC = (): JSX.Element => {
     const { data } = useProfile();
-    const { mutate: logout } = useLogout();
     return (
         <header
             className={cn(styles.header)}
         >
-            <Avatar onClick={() => logout()} avatarPath={data?.avatarPath} />
+            <Avatar avatarPath={data?.avatarPath} />
         </header>
     );
 }
