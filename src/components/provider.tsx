a2a2df4ead0e0eb04384f274c15f1db4ebdbf85f
Sorokovsky <Sorokovskys@ukr.net>
2025-01-03 20:53:35 +0200

    optimize

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -4,7 +4,13 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Provider: FC<PropsWithChildren> = ({ children }): JSX.Element => {
-    const [queryClient] = useState((): QueryClient => new QueryClient());
+    const [queryClient] = useState((): QueryClient => new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false
+            }
+        }
+    }));
     return (
         <QueryClientProvider client={queryClient} >
             {children}
