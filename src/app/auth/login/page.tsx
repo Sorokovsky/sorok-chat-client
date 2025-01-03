984a5baecfe0b126af8e5733ad6bc16a02807ae4
Sorokovsky <Sorokovskys@ukr.net>
2025-01-03 21:13:36 +0200

    start login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import {type NextPage} from "next";
+import {type JSX} from "react";
+
+const LoginPage: NextPage = (): JSX.Element => {
+    return (
+      <>
+          <h1>Login</h1>
+      </>
+    );
+}
+export default LoginPage;
