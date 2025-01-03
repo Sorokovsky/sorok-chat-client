dfa0e57c105bc7a87c160277a598893fe520d86a
Sorokovsky <Sorokovskys@ukr.net>
2025-01-03 21:39:46 +0200

    start login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,16 @@
+"use client"
 import {type NextPage} from "next";
-import {type JSX} from "react";
+import {type JSX, useEffect} from "react";
+import {useIsAuth} from "@/hooks/use-is-auth.hook";
+import {useRouter} from "next/navigation";
+import {type AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 const LoginPage: NextPage = (): JSX.Element => {
+    const isAuth: boolean = useIsAuth();
+    const router: AppRouterInstance = useRouter();
+    useEffect(() => {
+        if(isAuth) router.back();
+    }, [isAuth]);
     return (
       <>
           <h1>Login</h1>
--- src/app/auth/logout/page.tsx
+++ src/app/auth/logout/page.tsx
@@ -4,14 +4,13 @@ import {type JSX, useEffect} from "react";
 import {useLogout} from "@/hooks/use-logout";
 import {useRouter} from "next/navigation";
 import {type AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
-import {pagesService} from "@/services/pages.service";
 
 const Logout: NextPage = (): JSX.Element => {
     const { mutate: logout } = useLogout();
     const router: AppRouterInstance = useRouter();
     useEffect(() => {
         logout();
-        router.replace(pagesService.home);
+        router.back();
     }, []);
     return (
         <></>
