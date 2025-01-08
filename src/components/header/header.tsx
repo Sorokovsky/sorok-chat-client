4628ca8e90ed6baa4a1f02a46eea3c03628e4d08
Sorokovsky <Sorokovskys@ukr.net>
2025-01-08 18:33:34 +0200

    change avatar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,14 +4,16 @@ import cn from 'clsx';
 import styles from "@/components/header/header.module.sass"
 import { CurrentUserAvatar } from "@/components/current-user-avatar/current-user-avatar";
 import { Logo } from "@/components/logo/logo";
+import { useSettingsStore } from '@/store/settings/settings.store';
 
 export const Header: FC = (): JSX.Element => {
+    const toggle = useSettingsStore(state => state.toggle);
     return (
         <header
             className={cn(styles.header)}
         >
             <CurrentUserAvatar />
-            <Logo />
+            <Logo onClick={toggle} />
         </header>
     );
 }
