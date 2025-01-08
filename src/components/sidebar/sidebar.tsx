a6902a1f720902a8a852ce2baf77afac1bbc6a29
Sorokovsky <Sorokovskys@ukr.net>
2025-01-08 17:20:41 +0200

    sidebar optimization

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren, type FC, type JSX } from "react";
 import cn from "clsx";
 import styles from "./sidebar.module.sass";
 
-export type Position = "left" | "right"
+export type Position = "left" | "right";
 
 interface Props extends PropsWithChildren {
     position?: Position;
