f1b95c255294548fdd2922b39386ac3d43029635
Sorokovsky <Sorokovskys@ukr.net>
2025-01-09 15:14:53 +0200

    queries notification

--- src/components/pages/logout/logout.tsx
+++ src/components/pages/logout/logout.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Loader } from "@/components/ui/loader/loader";
-import { useIsAuth } from "@/hooks/use-is-auth.hook";
 import { useLogout } from "@/hooks/use-logout";
 import { useEffect, type FC, type JSX } from "react";
 
