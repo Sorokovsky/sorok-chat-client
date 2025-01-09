6e8415e7ca47cee03dfbeb67e5c810ddfa590eeb
Sorokovsky <Sorokovskys@ukr.net>
2025-01-09 21:04:07 +0200

    refactor

--- src/services/channels.service.ts
+++ src/services/channels.service.ts
@@ -1,14 +1,11 @@
 import { GetChannel } from "@/types/models/channel/get-channel.type";
 import { api } from "@/utils/api";
-import toast from "react-hot-toast";
-import { SUCCESS_CHANNELS_LOADED } from '@/constants/messages.constant';
 
 class ChannelsService {
     private readonly baseUrl: string = "/channels";
 
     public async getAll(): Promise<GetChannel[]> {
         const response = await api.get<GetChannel[]>(this.baseUrl);
-        if (response) toast.success(SUCCESS_CHANNELS_LOADED);
         return response.data;
     }
 };
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,15 +1,12 @@
 import {GetUser} from "@/types/models/users/get-user.type";
 import {api} from "@/utils/api";
 import {AxiosResponse} from "axios";
-import toast from 'react-hot-toast';
-import { SUCCESS_AUTHENTICATED } from '@/constants/messages.constant';
 
 class UsersService {
     private readonly baseUrl: string = '/users';
     private readonly getMeUrl: string = `${this.baseUrl}/get-me`;
     public async getMe(): Promise<GetUser> {
         const response: AxiosResponse<GetUser> = await api.get(this.getMeUrl);
-        if (response) toast.success(SUCCESS_AUTHENTICATED);
         return response.data;
     }
 }
