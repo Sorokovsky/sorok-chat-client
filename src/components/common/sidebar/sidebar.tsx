562a89d06c4b43c164a5b7b15fdaaa923d2ef5e2
Sorokovsky <Sorokovskys@ukr.net>
2025-01-12 15:45:52 +0200

    finish

--- src/components/common/sidebar/sidebar.tsx
+++ src/components/common/sidebar/sidebar.tsx
@@ -1,9 +1,8 @@
 "use client"
 import type { SidebarSetting } from "@/types/store/sidebar-setting.type";
-import { type FC, type JSX, type PropsWithChildren, useRef } from "react";
+import type { FC, JSX, PropsWithChildren} from "react";
 import cn from "clsx";
-import styles from "./sidebar.module.sass"
-import { useAutoClose } from "@/hooks/api/common/use-auto-close.hook";
+import styles from "./sidebar.module.sass";
 
 interface Props extends PropsWithChildren {
     position: "left" | "right";
@@ -12,14 +11,11 @@ interface Props extends PropsWithChildren {
 }
 
 export const Sidebar: FC<Props> = ({ position, state, className = "", children }): JSX.Element => {
-    const ref = useRef(null);
-    useAutoClose(ref, state);
     return (
         <aside
             className={cn(styles.sidebar, className, styles[position], {
                 [styles.open]: state.isOpen
             })}
-            ref={ref}
         >
             {children}
         </aside>
