728bfac88cfd732ee6c927d1d7cf22379dd0435f
Sorokovsky <Sorokovskys@ukr.net>
2025-01-16 21:09:25 +0200

    feat: logout page

--- src/hooks/api/use-logout.hook.ts
+++ src/hooks/api/use-logout.hook.ts
@@ -7,6 +7,6 @@ export const useLogout = () => {
     return useMutation(
         [QueryKeys.LOGOUT],
         () => authService.logout(),
-        SUCCESS_LOGOUT
+        SUCCESS_LOGOUT,
     );
 }
