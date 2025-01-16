87b35ee222a37324cd0829be54fa9f54ecb40207
Sorokovsky <Sorokovskys@ukr.net>
2025-01-16 19:46:43 +0200

    refactor routes

--- src/contracts/constants/routes.constants.ts
+++ src/contracts/constants/routes.constants.ts
@@ -2,23 +2,24 @@ import type {MenuRoute} from "@/types/routes/menu-route.type";
 import {LogInIcon, LogOutIcon, UserPlus2Icon} from "lucide-react";
 import {pagesService} from "@/services/pages.service";
 
-export const NOT_AUTH_ROUTES: MenuRoute[] = [
-    {
-        icon: LogOutIcon,
-        title: "Вийти",
-        path: pagesService.logout,
-    },
-];
+export const LOGOUT_ROUTE: MenuRoute = {
+    icon: LogOutIcon,
+    title: "Вийти",
+    path: pagesService.logout,
+};
 
-export const FOR_USER_AUTH_ROUTES: MenuRoute[] = [
-    {
-        icon: LogInIcon,
-        title: "Увійти",
-        path: pagesService.login,
-    },
-    {
-        icon: UserPlus2Icon,
-        title: "Зареєструватися",
-        path: pagesService.register,
-    },
-];
+export const REGISTER_ROUTE: MenuRoute = {
+    icon: UserPlus2Icon,
+    title: "Зареєструватися",
+    path: pagesService.register,
+};
+
+export const LOGIN_ROUTE: MenuRoute = {
+    icon: LogInIcon,
+    title: "Увійти",
+    path: pagesService.login,
+};
+
+export const NOT_AUTH_ROUTES: MenuRoute[] = [REGISTER_ROUTE, LOGIN_ROUTE];
+
+export const FOR_USER_AUTH_ROUTES: MenuRoute[] = [LOGOUT_ROUTE];
