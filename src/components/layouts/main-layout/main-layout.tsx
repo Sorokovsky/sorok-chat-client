95a597a1bf8c20e836e046f46172988ea93df564
Sorokovsky <Sorokovskys@ukr.net>
2025-10-18 19:06:07 +0300

    login form

--- src/components/layouts/main-layout/main-layout.tsx
+++ src/components/layouts/main-layout/main-layout.tsx
@@ -2,7 +2,6 @@
 import {FC, JSX, PropsWithChildren} from "react";
 import "./main-layout.scss";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ChakraProvider} from "@chakra-ui/react"
 
 const queryClient: QueryClient = new QueryClient();
 
@@ -11,9 +10,7 @@ export const MainLayout: FC<PropsWithChildren> = ({children}): JSX.Element => {
         <html lang="en" suppressHydrationWarning>
             <body>
                 <QueryClientProvider client={queryClient}>
-                    <ChakraProvider>
-                        {children}
-                    </ChakraProvider>
+                    {children}
                 </QueryClientProvider>
             </body>
         </html>
--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -1,11 +1,78 @@
 "use client"
 import {NextPage} from "next";
-import {JSX} from "react";
+import {ChangeEvent, FormEvent, JSX, useState} from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Heading from "@mui/material/Typography";
+import styles from "./login.module.scss";
+import clsx from "clsx";
+import {useLogin} from "@/hooks/login.hook";
+import {LoginDto} from "@/contracts/login.contract";
+
 
 export const LoginPage: NextPage = (): JSX.Element => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const {mutate: login} = useLogin();
+    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // @ts-ignore
+        const result: LoginDto = {};
+        const formData = new FormData(event.currentTarget);
+        formData.forEach((value, key) => {
+            // @ts-ignore
+            result[key] = value;
+        });
+        console.log(result);
+        login(result);
+    };
     return (
-        <div className="login-page">
-            LOGIN
+        <div className={clsx("page", styles.page)}>
+            <Box
+                component={"form"}
+                autoComplete="off"
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    maxWidth: 700,
+                    width: "100%",
+                    margin: "auto",
+                    gap: 10
+                }}
+                onSubmit={onSubmit}
+            >
+                <Heading
+                    variant={"h3"}
+                    align={"center"}
+                >
+                    Форма авторизації
+                </Heading>
+                <TextField
+                    id="email"
+                    label="Електронна адреса"
+                    variant="outlined"
+                    name={"email"}
+                    type={"email"}
+                    autoComplete="off"
+                    value={email}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+                />
+                <TextField
+                    id="password"
+                    label="Пароль"
+                    variant="outlined"
+                    name={"password"}
+                    type={"password"}
+                    autoComplete="off"
+                    value={password}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+                />
+                <Button
+                    variant="contained"
+                    type={"submit"}
+                >Увійти</Button>
+            </Box>
         </div>
     );
 }
