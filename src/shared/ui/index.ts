f1de0e1d7dd56092fcb8b383c85822eb78315260
Sorokovsky <Sorokovskys@ukr.net>
2025-10-25 15:02:40 +0300

    refactor

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,4 @@
 export { Navigation } from "./navigation/navigation";
 export { NavigationItem } from "./navigation/navigation-item/navigation-item";
 export { Form } from "./form/form";
-export { InputProps } from "./form/input/input.props";
+export type { InputProps } from "./form/input/input.props";
