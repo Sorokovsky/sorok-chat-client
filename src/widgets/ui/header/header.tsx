c66dd16e70e54618cd052b4632a5a473b3827f74
Sorokovsky <Sorokovskys@ukr.net>
2025-10-25 21:32:36 +0300

    add user indicator

--- src/widgets/ui/header/header.tsx
+++ src/widgets/ui/header/header.tsx
@@ -3,8 +3,14 @@ import type { FC, JSX } from "react";
 import clsx from "classnames";
 import styles from "./header.module.scss";
 import { Navigation } from "@/shared";
+import { useGetProfile, User } from "@/features/authentication";
+
+function formatUserName(user: User): string {
+    return `${user.lastName} ${user.firstName[0]}.${user.middleName[0]}.`;
+}
 
 export const Header: FC = (): JSX.Element => {
+    const { data: user } = useGetProfile();
     return (
         <AppBar
             position="sticky"
@@ -14,6 +20,7 @@ export const Header: FC = (): JSX.Element => {
                 className={clsx(styles.container)}
             >
                 <Navigation />
+                {user && <p>{formatUserName(user)}</p>}
             </Toolbar>
         </AppBar>
     );
