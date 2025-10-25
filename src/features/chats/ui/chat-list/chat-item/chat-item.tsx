5071fcb31f5c0a193cca0baec03345eb762f9970
Sorokovsky <Sorokovskys@ukr.net>
2025-10-25 15:54:40 +0300

    chat list

--- src/features/chats/ui/chat-list/chat-item/chat-item.tsx
+++ src/features/chats/ui/chat-list/chat-item/chat-item.tsx
@@ -3,9 +3,9 @@ import type { ChatItemProps } from "./chat-item.props";
 import clsx from "classnames";
 import styles from "./chat-item.module.scss";
 
-export const ChatItem: FC<ChatItemProps> = ({chat}): JSX.Element => {
+export const ChatItem: FC<ChatItemProps> = ({ chat }): JSX.Element => {
     return (
-        <li className={clsx(styles.item)}>
+        <li className={clsx(styles.item)} >
             <strong>{chat.name}</strong>
             <p>{chat.description}</p>
         </li>
