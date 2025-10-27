62d1b49b2a871a860a9850cc466e06095c0034e4
Sorokovsky <Sorokovskys@ukr.net>
2025-10-27 13:43:46 +0200

    fix format date

--- src/shared/lib/format-date.ts
+++ src/shared/lib/format-date.ts
@@ -1,6 +1,6 @@
-import { formatRelative, subDays } from "date-fns";
+import { format } from "date-fns";
 import { uk } from "date-fns/locale";
 
 export function formatDate(date: Date): string {
-    return formatRelative(subDays(new Date(), 3), date, {locale: uk});
+    return format(date, "yyy.MM.dd HH:mm", {locale: uk});
 };
