c0e88831c0f1075253a31d70062b3482a592804e
Sorokovskys <Sorokovskys@ukr.net>
2025-10-29 10:54:32 +0200

    refactor

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -3,7 +3,7 @@ import { FC, JSX } from "react";
 import { createBrowserRouter, RouterProvider as Provider, RouteObject } from "react-router";
 import { HOME_ROUTE } from '@/shared/routes/';
 import { Layout } from "./layout";
-import { NEW_USER_ROUTE } from '../../shared/routes/routes';
+import { NEW_USER_ROUTE } from '@/shared/routes/routes';
 
 const routerObjects: RouteObject[] = ROUTES.map(route => {
     return {
