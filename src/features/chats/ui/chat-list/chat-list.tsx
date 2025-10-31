361605251fd817c936d1f0f3323c2f7699c2be88
Sorokovskys <Sorokovskys@ukr.net>
2025-10-31 19:29:06 +0200

    refactor

--- src/features/chats/ui/chat-list/chat-list.tsx
+++ src/features/chats/ui/chat-list/chat-list.tsx
@@ -4,11 +4,13 @@ import clsx from 'classnames';
 import styles from "./chat-list.module.scss";
 import {Typography} from "@mui/material";
 import {ChatItem} from "./chat-item/chat-item";
+import {CREATE_CHAT_ROUTE, NavigationItem} from "@/shared";
 
 export const ChatList: FC<ChatListProps> = ({title, chats}): JSX.Element => {
     return (
         <div className={clsx(styles.chats)}>
             <Typography variant="h5">{title}</Typography>
+            <NavigationItem route={CREATE_CHAT_ROUTE}/>
             <ul>
                 {chats.map(chat => <ChatItem key={chat.id} chat={chat} />)}
             </ul>
